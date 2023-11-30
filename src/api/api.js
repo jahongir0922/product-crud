8faf1038edd74de27bd023024cc93fe426e5d6cc
jahongir0922 <jahongir0922@gmail.com>
2023-11-30 13:05:52 +0500

    saved

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,3 @@
 import axios from "axios";
-axios.defaults.baseURL = "https://94.158.54.194:9092/api/";
+axios.defaults.baseURL = "http://94.158.54.194:9092/api/";
 // axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token')
